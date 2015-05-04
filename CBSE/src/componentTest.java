e6b344e24d7d0783d4030dc5f14dba25ede11d68
GuyHero <taylor@guyhero.com>
2015-05-04 23:03:36 +0800

    Proof of concept for WAV loader.

--- CBSE/src/componentTest.java
+++ CBSE/src/componentTest.java
@@ -20,22 +20,24 @@ public class componentTest {
 	public static void main(String[] args) {
 		// Get audio stream file.
 		String fileName = "samples/1A5.wav";
-		File audioFile = new File(fileName);
+		WaveFile waveFile = new WaveFile();
+		waveFile.ReadInWAV(fileName);
 		
+		/*
 		try {
 			// Select audio stream from file system and read in information.
 			AudioInputStream inputStream = AudioSystem.getAudioInputStream(audioFile);
 			System.out.println("Opened audio stream " + inputStream.getFormat());
 			// Store .WAV information.
 			/* AudioInputStream seems to be unsuitable for getting WAV data as it searches for readable chunks.
-			 * May need to use an alternate scanner to skip a certain amount of bytes to the WAV data so it
-			 * can be read into a byte array.
-			 */
+			/* May need to use an alternate scanner to skip a certain amount of bytes to the WAV data so it
+			/* can be read into a byte array.
+			 
 			
 		
 		} catch(Exception e) {
 			System.out.println("Unable to open audio stream.");
-		}
+		} */
 	}
 	
 	
